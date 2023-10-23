e55e45aa3afcf9eba77e5e8f432fd318209acc0e
Vitaly Rumyancev <vitaly.rumyancev@appflair.io>
2023-10-23 13:54:41 +0300

    Работает локально.

--- index.js
+++ index.js
@@ -6,29 +6,55 @@ const path = require('path');
 const { createServer } = require('http');
 
 const WebSocket = require('ws');
+const secretTokens = new Map();
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
+app.get('/token', (req, res) => {
+    //http://localhost:8080/token/?token=630722&port=4444  
+    const token = req.query.token;
+    const ip = req.query.port;
+    console.log("Mobile send token: ", token, " ip: ", ip);  
+    const foundClient = findClientByToken(token); 
 
+    if (foundClient) {
+        foundClient.send(ip);
+        console.log("Found ws client, redirect to: ", ip);
+    } else {
+        console.log("Client ws not found");
+    }
+    res.send('GET request handled');
+  });
 const server = createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ server }); 
 
-wss.on('connection', function (ws) {
-  const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
-      //
-      // Ignoring errors.
-      //
+wss.on('connection', function (ws) { 
+  console.log('client new: ');
+  ws.onmessage = function(event) {
+      const token = event.data; 
+      if (secretTokens.has(token)) { 
+        console.log('token already is used:', token);
+      } else {
+        console.log('add token', token);
+        secretTokens.set(token, ws); 
+      }
+  }; 
+  ws.on("close", () => { 
+    console.log("close client, tokens ", secretTokens);
+    secretTokens.forEach((client, token) => {
+      if (client === ws) {
+        console.log("remove Client with token: ", token); 
+        secretTokens.delete(token);
+      }
     });
-  }, 100);
-  console.log('started client interval');
-
-  ws.on('close', function () {
-    console.log('stopping client interval');
-    clearInterval(id);
   });
 });
 
-server.listen(443, function () {
-  // console.log('Listening on 5443');
+var port = 8080
+server.listen(port, function () {
+  console.log('Listening on', port);
 });
+
+function findClientByToken(token) {
+  return secretTokens.get(token);
+}
