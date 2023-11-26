810ed4645e0b7fd0dfa0746e74e261e0929c6032
Vitaly Rumyancev <vitaly.rumyancev@appflair.io>
2023-11-26 02:25:33 +0200

    -

--- index.js
+++ index.js
@@ -1,85 +1,110 @@
+const http = require('http');
+const socketIo = require('socket.io');
 
-'use strict';
-
-const express = require('express');
-const path = require('path');
-const { createServer } = require('http');
-
-const WebSocket = require('ws');
-const secretTokens = new Map();
-//const unauthorizedClients = new Set();
-
-const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.json());
-
-app.post('/redirect', (req, res) => {
-    const authToken = req.headers.authorization;
-    
-    if (!authToken || authToken !== 'cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ') {
-        const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-//        unauthorizedClients.add(clientIP);
-        console.log("Unauthorized client: ", clientIP);
-        return res.status(401).json({ error: 'unauthorized' });
-    }
-    
-    const requestBody = req.body;
-    
-    const qrcode = requestBody.qrcode;
-    if (!qrcode) {
-        return res.status(401).json({ error: 'Error 3' });
-    }
-    
-    const client = findClientByToken(qrcode);
-    if (!client) {
-        logMessage("client not found for code: " + qrcode);
-        return res.status(401).json({ error: 'Client not found' });
-    }
-     
-    const redirectUrl = requestBody.url;
-    if (redirectUrl) {
-        logMessage("redirect client to url: " + redirectUrl);
-        client.send(redirectUrl);
-        res.json({ message: 'Success, redirected' });
-    } else {
-        res.json({ message: 'Success' });
-    }
-     
+// Создание HTTP сервера
+console.log('> Start: ' + socket.id);
+const server = http.createServer((req, res) => {
+    // Обработка HTTP запросов
+    // ...
+    console.log('> Request: ' + socket.id);
 });
 
+const io = socketIo(server);
 
-const server = createServer(app);
-const wss = new WebSocket.Server({ server });
-wss.on('connection', function (ws, req) {
-    const clientAddress = req.socket.remoteAddress;
-    logMessage('client new: ' + clientAddress);
-    
-    ws.onmessage = function(event) {
-        const token = event.data;
-        secretTokens.set(ws, token);
-        logMessage('add token: ' + token + ", client: " + clientAddress);
-    };
-    
-    ws.on("close", () => {
-        let success = secretTokens.delete(ws);
-        logMessage("disconnect client: " + clientAddress + ", removed from storage: " + success);
+const clients = new Map();
+
+io.on('connection', (socket) => {
+    console.log('Новое подключение: ' + socket.id);
+
+    // Сохранение ссылки на каждый сокет с его уникальным ID
+    clients.set(socket.id, socket);
+
+    socket.on('disconnect', () => {
+        clients.delete(socket.id);
+        console.log('Клиент отключился: ' + socket.id);
     });
 });
 
-var port = 443;
-server.listen(port, function () {
-    console.log("Run");
+// Функция для отправки сообщения клиенту
+function sendToClient(codeString, url) {
+    clients.forEach((client, clientId) => {
+        client.emit(codeString, { message: url });
+    });
+}
+
+// Здесь вы можете добавить обработку ваших HTTP запросов или другой логики
+
+const port = 443;
+server.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
 });
 
-function findClientByToken(text) {
-    for (const [client, token] of secretTokens.entries()) {
-        if (text === token) {
-            return client;
-        }
-    }
-    return null;
-}
+// const http = require('http');
+// const socketIo = require('socket.io');
+// const path = require('path');
+// const fs = require('fs');
 
-function logMessage(message) {
-//    console.log(message);
-}
+// const secretTokens = new Map();
+
+// // Создание HTTP сервера
+// const server = http.createServer((req, res) => {
+//     if (req.method === 'GET' && req.url === '/') {
+//         fs.readFile(path.join(__dirname, '/public/index.html'), (err, data) => {
+//             if (err) {
+//                 res.writeHead(404);
+//                 res.end('Error: File Not Found');
+//             } else {
+//                 res.writeHead(200, { 'Content-Type': 'text/html' });
+//                 res.end(data);
+//             }
+//         });
+//     }
+// });
+
+// // Подключение Socket.IO
+// const io = socketIo(server);
+
+// io.on('connection', (socket) => {
+//     logMessage('client new: ' + socket.id);
+
+//     socket.on('token', (token) => {
+//         secretTokens.set(socket.id, token);
+//         logMessage('add token: ' + token + ", client: " + socket.id);
+//     });
+
+//     socket.on('disconnect', () => {
+//         let success = secretTokens.delete(socket.id);
+//         logMessage("disconnect client: " + socket.id + ", removed from storage: " + success);
+//     });
+
+//     // Для обработки запроса на перенаправление
+//     socket.on('redirect', (data) => {
+//         const token = data.token;
+//         const redirectUrl = data.url;
+//         const client = findClientByToken(token);
+
+//         if (client && redirectUrl) {
+//             logMessage("redirect client to url: " + redirectUrl);
+//             io.to(client).emit('redirect', redirectUrl);
+//         }
+//     });
+// });
+
+// // Найти клиента по токену
+// function findClientByToken(token) {
+//     for (const [id, storedToken] of secretTokens.entries()) {
+//         if (token === storedToken) {
+//             return id;
+//         }
+//     }
+//     return null;
+// }
+
+// function logMessage(message) {
+//     console.log(message);
+// }
+
+// var port = 443;
+// server.listen(port, () => {
+//     console.log("Server running on port " + port);
+// });
