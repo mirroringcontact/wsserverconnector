abe127bcc8f36b1a2a3da29a339e21c0d028ab45
Vitaly Rumyancev <vitaly.rumyancev@appflair.io>
2023-12-13 20:46:50 +0200

    Pooling logic.

--- common/socket.js
+++ common/socket.js
@@ -4,8 +4,6 @@ const socketIO = require('socket.io');
 const { logger: logger } = require('../middleware/logger');
 
 let io;
-const secretTokens = new Map();
-
 const getSocketIO = function () {
   return io;
 };
@@ -23,35 +21,19 @@ const onConnectionSocketIO = function (io, socket) {
   const clientAddress = socket.handshake.address;
   logger.info(`client new: ${clientAddress}`);
 
-  socket.on('token', (token) => {
-    secretTokens.set(socket.id, token);
-    logger.info(`add token: ${token}, client: ${clientAddress}`);
-  });
-
   socket.on('disconnect', () => {
-    let success = secretTokens.delete(socket.id);
-    logger.info(`disconnect client: ${clientAddress}, removed from storage: ${success}`);
+    logger.info(`disconnect client: ${clientAddress}`);
   });
 };
 
-const sendRedirectURLToClient = function (socketId, codeString, redirectUrl) {
-  io.to(socketId).emit(codeString, redirectUrl);
+const sendRedirectURLToClient = function (codeString, redirectUrl) {
+  io.emit(codeString, redirectUrl);
   logger.info(
-    `send redirect url to socketID: ${socketId}, codestring: ${codeString}, redirectURL: ${redirectUrl}`,
+    `send redirect url for codestring: ${codeString}, redirectURL: ${redirectUrl}`,
   );
-};
-
-const findClientIdWithCode = function (text) {
-  for (const [client, codeString] of secretTokens.entries()) {
-    if (text === codeString) {
-      return client;
-    }
-  }
-  return null;
 };
 
 module.exports.getSocketIO = getSocketIO;
 module.exports.createSocketIO = createSocketIO;
 module.exports.onConnectionSocketIO = onConnectionSocketIO;
-module.exports.findClientIdWithCode = findClientIdWithCode;
 module.exports.sendRedirectURLToClient = sendRedirectURLToClient;
--- controllers/redirect.js
+++ controllers/redirect.js
@@ -15,15 +15,9 @@ exports.checkRedirect = async (req, res, next) => {
       return res.status(401).json({ error: 'Error 3' });
     }
 
-    const socketId = commonSocket.findClientIdWithCode(req.body.qrcode);
-    if (!socketId) {
-      logger.info(`client not found for code: ${req.body.qrcode}`);
-      return res.status(401).json({ error: 'Client not found' });
-    }
-
     if (req.body.url) {
       logger.info(`redirect client to url: ${req.body.url}`);
-      commonSocket.sendRedirectURLToClient(socketId, req.body.qrcode, req.body.url);
+      commonSocket.sendRedirectURLToClient(req.body.qrcode, req.body.url);
       res.json({ message: 'Success, redirected' });
     } else {
       res.json({ message: 'Success' });
